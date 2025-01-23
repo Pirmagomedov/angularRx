bcf92f465e12e1977d043b8cc6b08d23116a27ba
Pirmagomedov <gergotavra055@mail.ru>
2025-01-23 16:10:36 +0300

    ds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Subject, map, tap } from 'rxjs';
+import { Aa } from './a';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, Aa],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'alishka';
+  f = [1,2,3,4,5]
+  int: number = 0
+  bool: boolean = true
+  count$ = new Subject<number>()
+  constructor() {
+    this.count$.pipe(
+      tap(console.log)
+    )
+      .subscribe(
+        v => this.int = v
+      )
+
+    this.count$.pipe(
+    )
+      .subscribe(
+        v => this.bool = v % 2 === 0
+      )
+  }
+
+  a(a: HTMLDivElement) {
+    console.log(a, this)
+  }
+
+  d() {
+    this.count$.next(this.int + 1)
+  }
 }
